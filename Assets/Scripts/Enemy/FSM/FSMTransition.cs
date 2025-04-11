ff93a43bf55a7fb7684ba4fb40235f9227be3636
merthann <merthandurdag@gmail.com>
2025-04-11 16:17:26 +0300

    Created enemy sprites and AI coding for enemy brain

---
+++ Assets/Scripts/Enemy/FSM/FSMTransition.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+[Serializable]
+public class FSMTransition
+{
+    public FSMDecision Decision; //playerinrangeof -> T or F
+    public String TrueState;
+    public string FalseState;
+}
